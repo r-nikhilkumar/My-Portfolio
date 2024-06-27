af225231c9a735d0298dbec60bdced81463e5a2b
Nikhil Kumar <thenikhilkumar1@gmail.com>
2024-06-27 13:42:24 +0530

    done

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,7 @@ export const linkedin = "https://www.linkedin.com/in/r--nikhilkumar/"
 
 export const git_baseURL = "https://api.github.com";
 export const git_username = "r-nikhilkumar";
-export const git_token = "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";
+export const git_token = "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";
+const old_git_token = "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"
 
 export const backendUrl = "https://nikhilkumar.onrender.com"
