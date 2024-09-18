05b5343d82e5cdfbdc9cf4c228933b5cd11e7198
Nikhil Kumar <thenikhilkumar1@gmail.com>
2024-09-18 23:49:42 +0530

    done

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
-import mypic from "../../img/mypicture.png";
+import mypic from "../../img/mypic2.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
